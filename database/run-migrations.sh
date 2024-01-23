7a436ca27f70195c8fa319703c02be0c34b49b60
Santiago Baldassin <140439001+balda-rdx@users.noreply.github.com>
2024-01-23 14:09:22 -0300

    avoid dropping the db. create if not exist (#57)

--- database/run-migrations.sh
+++ database/run-migrations.sh
@@ -2,11 +2,15 @@
 DB_USERNAME="postgres"
 DB_NAME="dags"
 
-echo "Dropping existing database..."
-PGPASSWORD="$DB_PASSWORD" psql -h "$DB_HOST" -U $DB_USERNAME -c "DROP DATABASE IF EXISTS $DB_NAME;"
+echo "Checking if the database exists..."
+DB_EXISTS=$(PGPASSWORD="$DB_PASSWORD" psql -h "$DB_HOST" -U $DB_USERNAME -lqt | cut -d \| -f 1 | grep -w $DB_NAME)
 
-echo "Creating new database..."
-PGPASSWORD="$DB_PASSWORD" psql -h "$DB_HOST" -U $DB_USERNAME -c "CREATE DATABASE $DB_NAME;"
+if [ -z "$DB_EXISTS" ]; then
+    echo "Creating new database..."
+    PGPASSWORD="$DB_PASSWORD" psql -h "$DB_HOST" -U $DB_USERNAME -c "CREATE DATABASE $DB_NAME;"
+else
+    echo "Database already exists. Skipping creation."
+fi
 
 echo "Granting permissions..."
 PGPASSWORD="$DB_PASSWORD" psql -h "$DB_HOST" -U $DB_USERNAME -d $DB_NAME -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $DB_USERNAME;"
