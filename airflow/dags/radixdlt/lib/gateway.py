66cb88e4718d228634b80100d893fb01cadffeda
Santiago Baldassin <140439001+balda-rdx@users.noreply.github.com>
2024-03-11 18:05:00 +0100

    using cmc as primary source for compare (#188)
    * using cmc as primary source for compare
    
    * lint

--- airflow/dags/radixdlt/lib/gateway.py
+++ airflow/dags/radixdlt/lib/gateway.py
@@ -9,7 +9,7 @@ def is_transaction_committed(txn_intent_hash):
     retry_attempts = 0
     transaction_committed = False
     body = {"intent_hash": txn_intent_hash}
-    while retry_attempts < 6 and not transaction_committed:
+    while retry_attempts < 10 and not transaction_committed:
         txn_status = requests.post(
             url=f"{Config.NETWORK_GATEWAY}/transaction/status",
             json=body,
@@ -19,6 +19,6 @@ def is_transaction_committed(txn_intent_hash):
         if txn_status.status_code == 200:
             if txn_status.json()["status"] == "CommittedSuccess":
                 transaction_committed = True
-        sleep(10)
+        sleep(5)
         retry_attempts += 1
     return transaction_committed
--- airflow/dags/radixdlt/lib/oracle.py
+++ airflow/dags/radixdlt/lib/oracle.py
@@ -22,38 +22,33 @@ def update_oracle(coin_gecko_prices, cmc_prices, pyth_prices):
         logging.info(f"CMC price: {cmc_price}")
         logging.info(f"PYTH price: {pyth_prices[pair]}")
 
-        if gecko_price is not None:
-            logging.info("Gecko is not None. Will use gecko to compare pyth")
-            gecko_lowest_price = (
-                gecko_price - gecko_price * Config.ORACLE_PRICE_DIFF_TRIGGER
-            )
-            gecko_highest_price = (
-                gecko_price + gecko_price * Config.ORACLE_PRICE_DIFF_TRIGGER
-            )
-            if gecko_lowest_price < pyth_prices[pair] < gecko_highest_price:
-                logging.info(f"Gecko lower limit: {gecko_lowest_price}")
-                logging.info(f"Gecko higher limit: {gecko_highest_price}")
+        logging.info("Using CMC price as primary source. Using CMC to compare pyth")
+        if cmc_price is not None:
+            cmc_lowest_price = cmc_price - cmc_price * Config.ORACLE_PRICE_DIFF_TRIGGER
+            cmc_highest_price = cmc_price + cmc_price * Config.ORACLE_PRICE_DIFF_TRIGGER
+            if cmc_lowest_price < pyth_prices[pair] < cmc_highest_price:
+                logging.info(f"CMC lower limit: {cmc_lowest_price}")
+                logging.info(f"CMC higher limit: {cmc_highest_price}")
                 logging.info(f"Pyth price: {pyth_price}")
                 transaction_metadata.append(
                     {"base": pair.split("/")[0], "price": pyth_prices[pair]}
                 )
         else:
-            logging.info("Gecko price is None. Using CMC to compare pyth")
-            if cmc_price is not None:
-                cmc_lowest_price = (
-                    cmc_price - cmc_price * Config.ORACLE_PRICE_DIFF_TRIGGER
+            if gecko_price is not None:
+                logging.info("CMC price is None. using Gecko instead")
+                gecko_lowest_price = (
+                    gecko_price - gecko_price * Config.ORACLE_PRICE_DIFF_TRIGGER
                 )
-                cmc_highest_price = (
-                    cmc_price + cmc_price * Config.ORACLE_PRICE_DIFF_TRIGGER
+                gecko_highest_price = (
+                    gecko_price + gecko_price * Config.ORACLE_PRICE_DIFF_TRIGGER
                 )
-                if cmc_lowest_price < pyth_prices[pair] < cmc_highest_price:
-                    logging.info(f"CMC lower limit: {cmc_lowest_price}")
-                    logging.info(f"CMC higher limit: {cmc_highest_price}")
+                if gecko_lowest_price < pyth_prices[pair] < gecko_highest_price:
+                    logging.info(f"Gecko lower limit: {gecko_lowest_price}")
+                    logging.info(f"Gecko higher limit: {gecko_highest_price}")
                     logging.info(f"Pyth price: {pyth_price}")
                     transaction_metadata.append(
                         {"base": pair.split("/")[0], "price": pyth_prices[pair]}
                     )
-
     if len(transaction_metadata) > 0:
         logging.info(transaction_metadata)
         notarized_transaction_hex, address, txn_intent_hash = create_transaction(
