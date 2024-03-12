4ec61d580dad40430583d2655bfa66d6c025e5ab
Santiago Baldassin <140439001+balda-rdx@users.noreply.github.com>
2024-03-12 18:35:57 +0100

    send updates when there's at least one price (#193)
    * testing
    
    * lint
    
    * going back to 3 minutes

--- airflow/dags/radixdlt/lib/oracle.py
+++ airflow/dags/radixdlt/lib/oracle.py
@@ -1,6 +1,5 @@
 import logging
 import requests
-from airflow import AirflowException
 
 from radixdlt.config.config import Config
 from radixdlt.lib.ret import create_transaction
@@ -14,41 +13,46 @@ def update_oracle(coin_gecko_prices, cmc_prices, pyth_prices):
     for pair, pyth_price in pyth_prices.items():
         cmc_price = cmc_prices.get(pair, None)
         gecko_price = coin_gecko_prices.get(pair, None)
+
         if cmc_price is None and gecko_price is None:
-            raise AirflowException(f"Both gecko and cmc prices for {pair} are None")
-
-        logging.info(f"Checking pair: {pair}")
-        logging.info(f"Gecko price: {gecko_price}")
-        logging.info(f"CMC price: {cmc_price}")
-        logging.info(f"PYTH price: {pyth_prices[pair]}")
-
-        logging.info("Using CMC price as primary source. Using CMC to compare pyth")
-        if cmc_price is not None:
-            cmc_lowest_price = cmc_price - cmc_price * Config.ORACLE_PRICE_DIFF_TRIGGER
-            cmc_highest_price = cmc_price + cmc_price * Config.ORACLE_PRICE_DIFF_TRIGGER
-            if cmc_lowest_price < pyth_prices[pair] < cmc_highest_price:
-                logging.info(f"CMC lower limit: {cmc_lowest_price}")
-                logging.info(f"CMC higher limit: {cmc_highest_price}")
-                logging.info(f"Pyth price: {pyth_price}")
-                transaction_metadata.append(
-                    {"base": pair.split("/")[0], "price": pyth_prices[pair]}
-                )
+            logging.info(f"Both gecko and cmc prices for {pair} are None")
         else:
-            if gecko_price is not None:
-                logging.info("CMC price is None. using Gecko instead")
-                gecko_lowest_price = (
-                    gecko_price - gecko_price * Config.ORACLE_PRICE_DIFF_TRIGGER
+            logging.info(f"Checking pair: {pair}")
+            logging.info(f"Gecko price: {gecko_price}")
+            logging.info(f"CMC price: {cmc_price}")
+            logging.info(f"PYTH price: {pyth_prices[pair]}")
+
+            logging.info("Using CMC price as primary source. Using CMC to compare pyth")
+            if cmc_price is not None:
+                cmc_lowest_price = (
+                    cmc_price - cmc_price * Config.ORACLE_PRICE_DIFF_TRIGGER
                 )
-                gecko_highest_price = (
-                    gecko_price + gecko_price * Config.ORACLE_PRICE_DIFF_TRIGGER
+                cmc_highest_price = (
+                    cmc_price + cmc_price * Config.ORACLE_PRICE_DIFF_TRIGGER
                 )
-                if gecko_lowest_price < pyth_prices[pair] < gecko_highest_price:
-                    logging.info(f"Gecko lower limit: {gecko_lowest_price}")
-                    logging.info(f"Gecko higher limit: {gecko_highest_price}")
+                if cmc_lowest_price < pyth_prices[pair] < cmc_highest_price:
+                    logging.info(f"CMC lower limit: {cmc_lowest_price}")
+                    logging.info(f"CMC higher limit: {cmc_highest_price}")
                     logging.info(f"Pyth price: {pyth_price}")
                     transaction_metadata.append(
                         {"base": pair.split("/")[0], "price": pyth_prices[pair]}
                     )
+            else:
+                if gecko_price is not None:
+                    logging.info("CMC price is None. using Gecko instead")
+                    gecko_lowest_price = (
+                        gecko_price - gecko_price * Config.ORACLE_PRICE_DIFF_TRIGGER
+                    )
+                    gecko_highest_price = (
+                        gecko_price + gecko_price * Config.ORACLE_PRICE_DIFF_TRIGGER
+                    )
+                    if gecko_lowest_price < pyth_prices[pair] < gecko_highest_price:
+                        logging.info(f"Gecko lower limit: {gecko_lowest_price}")
+                        logging.info(f"Gecko higher limit: {gecko_highest_price}")
+                        logging.info(f"Pyth price: {pyth_price}")
+                        transaction_metadata.append(
+                            {"base": pair.split("/")[0], "price": pyth_prices[pair]}
+                        )
     if len(transaction_metadata) > 0:
         logging.info(transaction_metadata)
         notarized_transaction_hex, address, txn_intent_hash = create_transaction(
