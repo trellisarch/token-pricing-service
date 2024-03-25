d5f6a1b91737f216aff1042146c30cddd6d9b98a
Santiago Baldassin <140439001+balda-rdx@users.noreply.github.com>
2024-03-25 18:34:22 +0100

    removing test endpoints (#221)

--- consumers/token-price-service/app/main.py
+++ consumers/token-price-service/app/main.py
@@ -1,14 +1,10 @@
-from os import getenv
 from fastapi import FastAPI
 from app.api.price import price_router
-from app.api.test import test_router
 from app.api.token import tokens_router
 
 app = FastAPI()
 app.include_router(price_router, prefix="/price")
 app.include_router(tokens_router, prefix="/tokens")
-if getenv("ENVIRONMENT", "prod") != "dev":
-    app.include_router(test_router, prefix="/test")
 
 
 @app.get("/healthz")
