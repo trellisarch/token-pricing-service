5a44f8844b521149076a83c035b80c43edf3a838
Santiago Baldassin <140439001+balda-rdx@users.noreply.github.com>
2024-04-03 21:16:02 +0200

    Google store reviews metrics (#241)
    * add a dag to persist google play store ratings stats
    
    * lint

--- airflow/dags/radixdlt/dags/google_play/google_play.py
+++ airflow/dags/radixdlt/dags/google_play/google_play.py
@@ -10,6 +10,7 @@ from radixdlt.config.config import Config
 from airflow.decorators import task, dag
 
 from radixdlt.models.goodle_play.stats_install import GooglePlayInstalls
+from radixdlt.models.goodle_play.stats_ratings import GooglePlayRatings
 
 # DAG configuration
 default_args = {
@@ -58,7 +59,33 @@ def google_play_stats():
     def save_install_stats(stats_data_frame: DataFrame):
         GooglePlayInstalls.insert_csv_data(stats_data_frame)
 
+    @task
+    def get_ratings_stats():
+        # client = storage.Client(project=Config.GOOGLE_RADIX_PROJECT_ID)
+        # blobs = client.list_blobs(Config.GOOGLE_RADIX_STATS_BUCKET_ID, prefix="stats/installs")
+        # csv_blobs = [blob for blob in blobs if blob.name.endswith(".csv")]
+        # csv_blobs.sort(key=lambda blob: blob.creation_time, reverse=True)
+        # return pd.read_csv(csv_blobs[0], encoding="utf-8")
+
+        csv_file_path = join(
+            dirname(dirname(abspath(__file__))),
+            "google_play",
+            "stats_ratings.csv",
+        )
+
+        return pd.read_csv(
+            filepath_or_buffer=csv_file_path,
+            delimiter=",",
+            skipinitialspace=True,
+            encoding="utf-16",
+        )
+
+    @task
+    def save_ratings_stats(stats_data_frame: DataFrame):
+        GooglePlayRatings.insert_csv_data(stats_data_frame)
+
     save_install_stats(get_install_stats())
+    save_ratings_stats(get_ratings_stats())
 
 
 google_play_stats()
