ba9d3a526e74467129e4eae1b44f4cc2ec419cb7
marek-karwacki-rdx <118768577+marek-karwacki-rdx@users.noreply.github.com>
2024-07-22 14:35:00 +0200

    Merge develop to main (#288)

--- consumers/token-price-service/app/main.py
+++ consumers/token-price-service/app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 from app.api.price import price_router
 from app.api.token import tokens_router
+from prometheus_fastapi_instrumentator import Instrumentator
+from prometheus_client import start_http_server
 
 app = FastAPI()
 app.include_router(price_router, prefix="/price")
 app.include_router(tokens_router, prefix="/tokens")
+
+start_http_server(5990)
+Instrumentator().instrument(app)
 
 
 @app.get("/healthz")
